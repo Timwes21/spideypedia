f3fab267c09cb2dcbc5b3bcaa0dcd66d6a04791f
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-04-29 21:22:57 -0400

    added preflight cors part 3

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ import { redisPub, redisSub } from './utils/redis.js';
 const app = express();
 const server = http.createServer(app);
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' , credentials: true}));
+app.use(cors({ 
+    origin: 'http://localhost:5173', 
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.options('*', cors());
 
 ws(server, getCharacters, redisSub, productionCollection);
