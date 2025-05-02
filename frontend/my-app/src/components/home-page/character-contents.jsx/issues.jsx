33fa9def96372e716e29492cef51e6f8c8d7e6a6
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-05-01 20:22:05 -0400

    fixed folder name issue part 2

--- frontend/my-app/src/components/home-page/character-contents.jsx/issues.jsx
+++ frontend/my-app/src/components/home-page/character-contents.jsx/issues.jsx
@@ -18,7 +18,7 @@ export default function Issues({character, type, titleName, vol, issues, visible
         <div className="issues">
             {Object.entries(issues).map(([issueNumber, issueDetails])=>(
                 <div key={issueNumber} className="issues">
-                    <span className="char-header" onClick={()=>toggleVisibility(issueNumber)}>{issueNumber}: {()=>issueDetails.Name?issueDetails.Name:""}</span>
+                    <span className="char-header" onClick={()=>toggleVisibility(issueNumber)}>{issueNumber}: {issueDetails.Name?issueDetails.Name:""}</span>
                     <IssueDetails character={character} type={type} titleName={titleName} vol={vol} issueNumber={issueNumber} issueDetails={issueDetails} visible={issueDetailsVisibility[issueNumber]}/>
                     <DeleteIssue character={character} type={type} titleName={titleName} vol={vol} issueNumber={issueNumber}/>
                     <hr/>
