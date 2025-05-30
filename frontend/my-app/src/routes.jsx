1e2f02e5be9b84fff396ae6abf837f6cf3ed8bc0
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-05-29 20:03:14 -0400

    forgot to change developing to false

--- frontend/my-app/src/routes.jsx
+++ frontend/my-app/src/routes.jsx
@@ -1,4 +1,4 @@
-const developing = true;
+const developing = false;
 const base = developing?"http://localhost:3000":"https://spideypedia-production.up.railway.app";
 const wsLink = developing?"ws://localhost:3000":"wss://spideypedia-production.up.railway.app";
 const comicsBase = base + "/comics";
