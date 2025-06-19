cef03248d5dd01f557f477ea871b676260cc191a
Timothy Wesley <timothy.wesley99@yahoo.com>
2025-06-18 23:08:46 -0400

    forgot to turn off custom dev mode

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,5 +1,5 @@
 
-const developing = true;
+const developing = false;
 const base = developing?"http://localhost:3000":"https://spideypedia-production.up.railway.app";
 const wsLink = developing?"ws://localhost:3000":"wss://spideypedia-production.up.railway.app";
 const comicsBase = base + "/comics";
