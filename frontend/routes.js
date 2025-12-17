a3a3d4f02df9887fb6a1fe0f8f437ef96787d791
Timothy Wesley <timothywesley@mac.mynetworksettings.com>
2025-12-17 01:30:05 -0500

    fixed the routing

--- frontend/routes.js
+++ frontend/routes.js
@@ -5,11 +5,12 @@ const testingAIBackendBase = import.meta.env.VITE_AI_BASE_ROUTE;
 console.log(testingBackendBase);
 
 const http = developing == "true" ?"http": "https";
+const ws = developing == "true"? "ws": "wss"
 const backendBase = `${http}://${testingBackendBase}`;
 const aiBackend = `${http}://${testingAIBackendBase}`;
 
 
-const wsLink = developing? `ws://${testingBackendBase}`: "";
+const wsLink = `${ws}://${testingBackendBase}`;
 const authBase = backendBase + "/auth";
 const RailwayAgent = "wss://intuitive-smile-production.up.railway.app/ws";
 const submitToAgentWs = developing?"ws://127.0.0.1:8000/ws":RailwayAgent;
